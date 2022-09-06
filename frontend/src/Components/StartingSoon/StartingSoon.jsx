6564baa0a148ee9f5a10ef7261ee8ec0d9cfe92a
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2022-09-06 23:02:26 +0500

    .

--- frontend/src/Components/StartingSoon/StartingSoon.jsx
+++ frontend/src/Components/StartingSoon/StartingSoon.jsx
@@ -93,91 +93,93 @@ const StartingSoon = () => {
     else setFlag1(key);
   }
   return (
-    <div className="StartingSoonContainer">
-      {Leagues.map((item, index) => (
-        <div id={index} className="League_StartingSoon">
-          <div id={index} className="MainMenu_StartingSoon">
-            <p className="LeagueName_StartingSoon">{item.Name}</p>
-            <img
-              className="dropDownIcon_StartingSoon"
-              onClick={toggle}
-              src={dropDownLogo}
-              alt="dropDownIcon"
-            />
-          </div>
-          {filteredMatches.map(
-            (item, index2) =>
-              flag == index && (
-                <div className="DashboardRoot">
-                  <div key={index2} className="DashboardMatch_StartingSoon">
-                    <div className="LeftMain_StartingSoon">
-                      <p className="UpcomingText_StartingSoon">Upcoming</p>
+    <div className="StartSoon_root_div">
+      <div className="StartingSoonContainer">
+        {Leagues.map((item, index) => (
+          <div id={index} className="League_StartingSoon">
+            <div id={index} className="MainMenu_StartingSoon">
+              <p className="LeagueName_StartingSoon">{item.Name}</p>
+              <img
+                className="dropDownIcon_StartingSoon"
+                onClick={toggle}
+                src={dropDownLogo}
+                alt="dropDownIcon"
+              />
+            </div>
+            {filteredMatches.map(
+              (item, index2) =>
+                flag == index && (
+                  <div className="DashboardRoot">
+                    <div key={index2} className="DashboardMatch_StartingSoon">
+                      <div className="LeftMain_StartingSoon">
+                        <p className="UpcomingText_StartingSoon">Upcoming</p>
 
-                      <div className="TeamsName_StartingSoon">
-                        <p className="Team1Name_StartingSoon">
-                          {item.Team1Name}
-                        </p>
-                        <p className="Team2Name_StartingSoon">
-                          {item.Team2Name}
-                        </p>
+                        <div className="TeamsName_StartingSoon">
+                          <p className="Team1Name_StartingSoon">
+                            {item.Team1Name}
+                          </p>
+                          <p className="Team2Name_StartingSoon">
+                            {item.Team2Name}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="RightMain_StartingSoon">
+                        <div className="Predictions_StartingSoon">
+                          {item.PredictionValue1 === "Suspended" ? (
+                            <>
+                              <div className="Team1Prediction_StartingSoon">
+                                <p className="TeamNameSuspended_StartingSoon">
+                                  {item.Team1Name}
+                                </p>
+                                <p className="PredictionValueSuspended_StartingSoon">
+                                  {item.PredictionValue1}
+                                </p>
+                              </div>
+                              <div className="Team2Prediction_StartingSoon">
+                                <p className="TeamNameSuspended_StartingSoon">
+                                  {item.Team2Name}
+                                </p>
+                                <p className="PredictionValueSuspended_StartingSoon">
+                                  {item.PredictionValue2}
+                                </p>
+                              </div>
+                            </>
+                          ) : (
+                            <>
+                              <div className="Team1Prediction_StartingSoon">
+                                <p className="TeamName_StartingSoon">
+                                  {item.Team1Name}
+                                </p>
+                                <p className="PredictionValue_StartingSoon">
+                                  {item.PredictionValue1}
+                                </p>
+                              </div>
+                              <div className="DrawPrediction_StartingSoon">
+                                <p className="DrawText_StartingSoon">Draw</p>
+                                <p className="PredictionValue_StartingSoon">
+                                  {item.DrawPredictionValue}
+                                </p>
+                              </div>
+                              <div className="Team2Prediction_StartingSoon">
+                                <p className="TeamName_StartingSoon">
+                                  {item.Team2Name}
+                                </p>
+                                <p className="PredictionValue_StartingSoon">
+                                  {item.PredictionValue2}
+                                </p>
+                              </div>
+                            </>
+                          )}
+                        </div>
                       </div>
                     </div>
-                    <div className="RightMain_StartingSoon">
-                      <div className="Predictions_StartingSoon">
-                        {item.PredictionValue1 === "Suspended" ? (
-                          <>
-                            <div className="Team1Prediction_StartingSoon">
-                              <p className="TeamNameSuspended_StartingSoon">
-                                {item.Team1Name}
-                              </p>
-                              <p className="PredictionValueSuspended_StartingSoon">
-                                {item.PredictionValue1}
-                              </p>
-                            </div>
-                            <div className="Team2Prediction_StartingSoon">
-                              <p className="TeamNameSuspended_StartingSoon">
-                                {item.Team2Name}
-                              </p>
-                              <p className="PredictionValueSuspended_StartingSoon">
-                                {item.PredictionValue2}
-                              </p>
-                            </div>
-                          </>
-                        ) : (
-                          <>
-                            <div className="Team1Prediction_StartingSoon">
-                              <p className="TeamName_StartingSoon">
-                                {item.Team1Name}
-                              </p>
-                              <p className="PredictionValue_StartingSoon">
-                                {item.PredictionValue1}
-                              </p>
-                            </div>
-                            <div className="DrawPrediction_StartingSoon">
-                              <p className="DrawText_StartingSoon">Draw</p>
-                              <p className="PredictionValue_StartingSoon">
-                                {item.DrawPredictionValue}
-                              </p>
-                            </div>
-                            <div className="Team2Prediction_StartingSoon">
-                              <p className="TeamName_StartingSoon">
-                                {item.Team2Name}
-                              </p>
-                              <p className="PredictionValue_StartingSoon">
-                                {item.PredictionValue2}
-                              </p>
-                            </div>
-                          </>
-                        )}
-                      </div>
-                    </div>
+                    {flag1 == index2 && <Stats />}
                   </div>
-                  {flag1 == index2 && <Stats />}
-                </div>
-              )
-          )}
-        </div>
-      ))}
+                )
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
